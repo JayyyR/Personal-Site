d9bbd3dd126d57a47213ce131334775738728c5e
unknown <flatpita92@gmail.com>
2015-10-03 18:48:30 -0500

    initial commit, working on main page

---
+++ static/js/init.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
